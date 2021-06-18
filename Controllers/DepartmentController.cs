f7531546627d82d18890343701549e4595d275d0
fazil9600 <fazil9600@gmail.com>
2021-06-18 12:23:36 +0530

    initial commit

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using LatestWEBAPI.Models;
+
+namespace LatestWEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //Adding an API method get all the department details
+        [HttpGet]
+
+        public JsonResult Get()
+        {
+            string query = @"
+                        select DepartmentID, DepartmentName from dbo.department";
+            DataTable table = new DataTable();
+            string sqldatasource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader  myReader;
+            using(SqlConnection myCon = new SqlConnection(sqldatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        //Adding a post method to insert DATA
+
+        [HttpPost]
+
+        public JsonResult Post(Department dep)
+        {
+            string query = @"
+                        insert into dbo.Department values ('"+dep.DepartmentName+@"')";
+            DataTable table = new DataTable();
+            string sqldatasource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqldatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Department Added Succesfully");
+        }
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LatestWEBAPI.Models
+{
+    public class Employee
+    {
+
+        public int EmployeeID { get; set; }
+
+        public string EmployeeName { get; set; }
+
+        public string Department { get; set; }
+
+        public string DateJointed { get; set; }
+
+        public string ProfileName { get; set; }
+
+    }
+}
+
+
+
+//EmployeeID EmployeeName	Department	DateJointed	ProfileName
+//1	Fazil	IT	2021-06-18	faz
+//2	Ruchika	Support	2021-06-18	ruchii
